8485367fe4b40a461ef71731263a5deccdfe5355
yamachu <yamachu.dev@gmail.com>
2025-03-22 21:38:41 +0900

    Add base project

---
+++ Examples/SampleApp/SampleApp/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by Yusuke Yamada on 2025/03/22.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
