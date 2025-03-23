575f611e399163618fafdbd04a1b0a16f745659d
yamachu <yamachu.dev@gmail.com>
2025-03-23 10:32:00 +0900

    Add impl

--- Examples/Package.swift
+++ Examples/Package.swift
@@ -5,17 +5,36 @@ import PackageDescription
 
 let package = Package(
     name: "SampleAppCore",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v15),
+        .macCatalyst(.v15),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SampleAppCore",
-            targets: ["SampleAppCore"]),
+            targets: ["SampleAppCore"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/mw99/DataCompression.git", from: "3.8.0"),
+        .package(url: "https://github.com/kayembi/Tarscape.git", branch: "main"),
+        .package(url: "https://github.com/yamachu/VoicevoxCoreSwift.git", branch: "main"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SampleAppCore"),
+            name: "SampleAppCore",
+            dependencies: [
+                "DataCompression",
+                "Tarscape",
+                "VoicevoxCoreSwift"
+            ],
+            resources: [
+                .process("Resources/models.json")
+            ]
+        ),
         .testTarget(
             name: "SampleAppCoreTests",
             dependencies: ["SampleAppCore"]
--- Examples/SampleApp/SampleApp/SampleAppApp.swift
+++ Examples/SampleApp/SampleApp/SampleAppApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SampleAppCore
 
 @main
 struct SampleAppApp: App {
